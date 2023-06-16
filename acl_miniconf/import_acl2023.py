63bedeba7480221ab7dd52c9520caff99343c0a3
Pedro Rodriguez <me@pedro.ai>
2023-06-16 12:12:11 -0700

    Working parser for posters and orals
    Summary:
    
    Test Plan:

--- acl_miniconf/import_acl2023.py
+++ acl_miniconf/import_acl2023.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import pandas as pd
 import pytz
 
-from acl_miniconf.data import Session, SessionEvent, Paper, Conference
+from acl_miniconf.data import Session, Event, Paper, Conference
 
 
 DATE_FMT = '%Y-%m-%d %H:%M'
@@ -41,7 +41,7 @@ class Acl2023Parser:
         self.oral_tsv_path = oral_tsv_path
         self.papers: Dict[str, Paper] = {}
         self.sessions: Dict[str, Session] = {}
-        self.session_events: Dict[str, SessionEvent] = {}
+        self.events: Dict[str, Event] = {}
         self.zone = pytz.timezone('America/Toronto')
     
     def parse(self):
@@ -50,7 +50,7 @@ class Acl2023Parser:
         return Conference(
             sessions=self.sessions,
             papers=self.papers,
-            session_events=self.session_events
+            events=self.events
         )
     
     def _parse_start_end_dt(self, date_str: str, time_str: str):
@@ -63,11 +63,11 @@ class Acl2023Parser:
         df = pd.read_csv(poster_tsv_path, sep='\t')
         group_type = 'Poster'
         for (group_session, group_track), group in df.groupby(['Session', 'Track']):
-            group = group.sort_values('Local Order')
+            group = group.sort_values('Local order')
             event_name = get_session_event_name(group_session, group_track, group_type)
             start_dt, end_dt = self._parse_start_end_dt(group.iloc[0].Date, group.iloc[0].Time)
-            if event_name not in self.session:
-                self.sessions[event_name] = SessionEvent(
+            if event_name not in self.events:
+                self.events[event_name] = Event(
                     id=event_name,
                     session=group_session,
                     track=group_track,
@@ -79,7 +79,7 @@ class Acl2023Parser:
                     room='Poster Session',
                     type=group_type,
                 )
-            event = self.session_event[event_name]
+            event = self.events[event_name]
             if group_session not in self.sessions:
                 self.sessions[group_session] = Session(
                     id=group_session,
@@ -92,11 +92,11 @@ class Acl2023Parser:
             if event_name in session.events:
                 raise ValueError('Duplicated events')
 
-            session.events[group_session] = event
+            session.events[event_name] = event
             for row in group.itertuples():
                 paper_id = row.PID
                 start_dt, end_dt = self._parse_start_end_dt(row.Date, row.Time)
-                event = self.session_events[event_name]
+                event = self.events[event_name]
                 event.paper_ids.append(paper_id)
 
 
@@ -105,21 +105,23 @@ class Acl2023Parser:
         group_type = 'Oral'
         for (group_session, group_track), group in df.groupby(['Session', 'Track']):
             group = group.sort_values('Track Order')
-            session_event_name = get_session_event_name(group_session, group_track, group_type)
+            room = group.iloc[0].Room
+            event_name = get_session_event_name(group_session, group_track, group_type)
             start_dt, end_dt = self._parse_start_end_dt(group.iloc[0].Date, group.iloc[0].Time)
-            if session_event_name not in self.session_events:
-                self.session_events[session_event_name] = SessionEvent(
-                    id=session_event_name,
+            if event_name not in self.events:
+                self.events[event_name] = Event(
+                    id=event_name,
+                    session=group_session,
                     track=group_track,
                     start_time=start_dt,
                     end_time=end_dt,
                     chairs=[],
                     paper_ids=[],
                     link=None,
-                    room=row.Room,
+                    room=room,
                     type=group_type,
                 )
-            event = self.session_events[session_event_name]
+            event = self.events[event_name]
             if group_session not in self.sessions:
                 self.sessions[group_session] = Session(
                     id=group_session,
@@ -129,7 +131,7 @@ class Acl2023Parser:
                     events=[],
                 )
             session = self.sessions[group_session]
-            session.events.append(event)
+            session.events[event_name] = event
             for row in group.itertuples():
                 paper_id = row.PID
                 event.paper_ids.append(paper_id)
@@ -145,9 +147,8 @@ class Acl2023Parser:
                     keywords=[],
                     pdf_url="",
                     demo_url="",
-                    session_event_ids=[event.id],
+                    event_ids=[event.id],
                     similar_paper_ids=[],
-                    program="",
                     forum="",
                     card_image_path="",
                     presentation_id="",

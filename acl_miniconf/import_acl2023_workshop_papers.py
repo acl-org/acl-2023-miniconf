f8df1273af542d1517daa4e19a4f347dd74f3704
EntilZha <me@pedro.ai>
2023-07-03 15:24:58 -0600

    Add workshop papers, need to add their event_ids still
    <!-- ps-id: 28ae5604-a612-4246-8195-ace3aa8dfc6b -->

---
+++ acl_miniconf/import_acl2023_workshop_papers.py
@@ -0,0 +1,98 @@
+from pathlib import Path
+import re
+from typing import List
+
+from rich.progress import track
+import yaml
+import typer
+
+from acl_miniconf.data import Paper, WORKSHOP
+from acl_miniconf.import_acl2023 import TLDR_LENGTH
+
+CUSTOM_PAPER_YML = {
+    Path('workshop-data/DISRPT'): Path('workshop-data/DISRPT/data'),
+    Path('workshop-data/LAW'): Path('workshop-data/LAW/inputs'),
+    Path('workshop-data/Narrative-Understanding'): Path('workshop-data/Narrative-Understanding/output/inputs/'),
+    Path('workshop-data/BEA'): Path('workshop-data/BEA/inputs'),
+    Path('workshop-data/IWSLT'): Path('workshop-data/IWSLT/inputs'),
+    Path('workshop-data/CODI'): Path('workshop-data/CODI/inputs'),
+}
+
+def load_papers(path: Path):
+    try:
+        with open(path) as f:
+            papers = yaml.full_load(f)
+    except yaml.scanner.ScannerError:
+        lines = []
+        with open(path) as f:
+            for line in f:
+                if "title:" in line:
+                    match = re.match(r"(\s+)title: (.+)\n", line)
+                    if match is None:
+                        raise ValueError()
+                    else:
+                        spaces = match.group(1)
+                        title = match.group(2)
+                        lines.append(f'{spaces}title: "{title}"\n')
+                elif "abstract:" in line:
+                    match = re.match(r"(\s+)abstract: (.+)\n", line)
+                    if match is None:
+                        raise ValueError()
+                    else:
+                        spaces = match.group(1)
+                        title = match.group(2)
+                        # Conference didn't write valid yml and I tried parsing it, don't have time to manually
+                        # fix every case, so just stripping abstracts out
+                        lines.append(f'{spaces}abstract: ""\n')
+                else:
+                    lines.append(line)
+        fixed_content = "".join(lines)
+        papers = yaml.safe_load(fixed_content)
+    return papers
+
+
+def main(workshop_data_dir: Path = Path('workshop-data'), output_dir: Path = Path('data/acl_2023/data')):
+    workshop_papers: List[Paper] = []
+    for workshop_dir in track(list(workshop_data_dir.glob('*'))):
+        if workshop_dir in CUSTOM_PAPER_YML:
+            workshop_dir = CUSTOM_PAPER_YML[workshop_dir]
+        if not (workshop_dir / 'papers.yml').exists():
+            raise ValueError(f'Workshop papers.yml does not exist for: {workshop_dir}')
+        if not (workshop_dir / 'conference_details.yml').exists():
+            raise ValueError(f'Workshop conference_details.yml does not exist for: {workshop_dir}')
+        
+        with open(workshop_dir / 'conference_details.yml') as f:
+            details = yaml.safe_load(f)
+            workshop_name = details['event_name']
+            prefix = details['anthology_venue_id']
+
+        papers = load_papers(workshop_dir / 'papers.yml')
+        for p in papers:
+            workshop_paper_id = p['id']
+            authors = [f"{a['first_name']} {a['last_name']}" for a in p['authors']]
+            if 'attributes' in p:
+                paper_type = p['attributes']['paper_type']
+            else:
+                paper_type = "long"
+            maybe_abstract = p.get('abstract', "")
+            workshop_papers.append(Paper(
+                id=f'{prefix}_{workshop_paper_id}',
+                forum="",
+                card_image_path="",
+                title=p['title'],
+                authors=authors,
+                track=workshop_name,
+                paper_type=paper_type,
+                category=WORKSHOP,
+                abstract=maybe_abstract if isinstance(maybe_abstract, str) else "",
+                tldr=maybe_abstract[:TLDR_LENGTH] if isinstance(maybe_abstract, str) else "",
+                event_ids=[],
+                program=WORKSHOP,
+            ).dict())
+        with open(output_dir / 'workshop_papers.yaml', 'w') as f:
+            yaml.dump(workshop_papers, f)
+
+
+
+if __name__ == '__main__':
+    typer.run(main)
---
+++ scripts/clone_workshops.sh
@@ -0,0 +1,25 @@
+mkdir -p workshop-data
+cd workshop-data
+gh repo clone ACL-2023/NLP4ConvAI
+gh repo clone ACL-2023/WOAH
+gh repo clone ACL-2023/Clinical-NLP
+gh repo clone ACL-2023/CODI
+gh repo clone ACL-2023/DISRPT
+gh repo clone ACL-2023/SEM
+gh repo clone ACL-2023/Narrative-Understanding
+gh repo clone ACL-2023/SemEval
+gh repo clone ACL-2023/MATCHING
+gh repo clone ACL-2023/RepL4NLP
+gh repo clone ACL-2023/CAWL
+gh repo clone ACL-2023/SICon
+gh repo clone ACL-2023/SustaiNLP
+gh repo clone ACL-2023/DialDoc
+gh repo clone ACL-2023/BioNLP-ST
+gh repo clone ACL-2023/BEA
+gh repo clone ACL-2023/NLRSE
+gh repo clone ACL-2023/AmericasNLP
+gh repo clone ACL-2023/IWSLT
+gh repo clone ACL-2023/SIGMORPHON
+gh repo clone ACL-2023/LAW
+gh repo clone ACL-2023/TrustNLP
+gh repo clone ACL-2023/WASSA

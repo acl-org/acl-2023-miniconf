d5311184f7df965c75db7bc86d2bffe81430f5bc
Martin Villalba <villalba@7c0h.com>
2023-07-03 14:07:30 +0200

    Adds new data resources to the website: booklet information, scripts for importing it, and images for the invited talks.

---
+++ acl_miniconf/import_booklet_acl2023.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import datetime
+import json
+from .site_data import Tutorial, SessionInfo, PlenarySession
+
+
+def generate_plenaries(plenaries_list):
+    plenaries = {}
+    for plenary_dict in plenaries_list:
+        day = datetime.datetime.fromisoformat(plenary_dict['start_time']).strftime("%A")
+        if day not in plenaries:
+            plenaries[day] = []
+        plenaries[day].append(PlenarySession(id=plenary_dict['id'],
+                                             title=plenary_dict['title'],
+                                             image=plenary_dict['image'],
+                                             day=day,
+                                             sessions=[],
+                                             presenter=plenary_dict['speaker_name'],
+                                             institution=plenary_dict['institution'],
+                                             abstract=plenary_dict['desc'],
+                                             bio=plenary_dict['bio'],
+                                             presentation_id=None,
+                                             rocketchat_channel=None,
+                                             videos=[]))
+    return plenaries
+
+
+def generate_tutorials(tutorial_list):
+    tutorials = []
+    for tutorial_dict in tutorial_list:
+        # Put the Tutorial event together
+        blocks = [SessionInfo(session_name=tutorial_dict['start_time'],
+                              start_time=datetime.datetime.fromisoformat(tutorial_dict['start_time']),
+                              end_time=datetime.datetime.fromisoformat(tutorial_dict['end_time']),
+                              hosts=tutorial_dict['hosts'],
+                              link=None)]
+        day = datetime.datetime.fromisoformat(tutorial_dict['start_time']).strftime("%A")
+        final_tutorial = Tutorial(id=tutorial_dict['id'],
+                                  title=tutorial_dict['title'],
+                                  organizers=', '.join(tutorial_dict['hosts']),
+                                  abstract=tutorial_dict['desc'],
+                                  rocketchat_channel=None,
+                                  sessions=[],
+                                  blocks=blocks,
+                                  virtual_format_description=None,
+                                  website=None,
+                                  material=None,
+                                  slides=None,
+                                  prerecorded=None
+                                  )
+        tutorials.append(final_tutorial)
+    return tutorials
